997fde7ff79c7fdf83bf71180693884e5795d131
Zhiling Chen <625676308@qq.com>
2026-01-29 00:16:07 -0500

    google verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+export const metadata = {
+  verification: {
+    google: "nldMm_6-MVdGvo4QP6eEdp0W7Dx_UjIEutWpkq7EpEw",
+  },
+};
+
+
 export default function RootLayout({
   children,
 }: {
